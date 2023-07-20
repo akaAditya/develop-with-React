78ff2e986863cb2be297013dbad506ee41c6a443
akaAditya <mymail.adityapatel@gmail.com>
2023-07-21 01:11:28 +0530

    first commit expense items

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ExpenseItems from './components/ExpenseItems'
+
+function App() {
+  return (
+    <div>
+      
+      <h1>Let's get started</h1>
+      <ExpenseItems></ExpenseItems>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ components/ExpenseItems.js
@@ -0,0 +1,15 @@
+function ExpenseItems() {
+  return (
+    <>
+      <div>
+        <h1>Expense Items!</h1>
+        <div>
+          <h2>Food Rs 10</h2>
+          <h2>Petrol Rs 100</h2>
+          <h2>Movies Rs 200</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+export default ExpenseItems;
