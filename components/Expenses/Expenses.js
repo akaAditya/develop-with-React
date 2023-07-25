71de40dcf1a5532b762414e2b6724e94ed6f4fbc
akaAditya <mymail.adityapatel@gmail.com>
2023-07-25 10:27:08 +0530

    fifteenth commit added filter dropdown

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItems from "./ExpenseItems";
 import Cards from "../UI/Cards";
 import "./Expense.css";
+import ExpensesFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const getFilteredYear = (selectedYear)=>{
+    setFilteredYear(selectedYear);
+  }
 
   return (
     <>
       <Cards className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter = {getFilteredYear}/>
         {Object.keys(props.items).map((expense, key) => {
           return (
             <ExpenseItems
